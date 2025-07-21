92d48ddd9b5681477199fe6c741703959523d8fa
axgiri <reiaogiri@gmail.com>
2025-07-21 13:30:18 +0200

    runtime fix

--- src/main/java/github/oldLab/oldLab/OldLabApplication.java
+++ src/main/java/github/oldLab/oldLab/OldLabApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@EnableAsync
+@EnableAsync(proxyTargetClass = true)
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class OldLabApplication {
