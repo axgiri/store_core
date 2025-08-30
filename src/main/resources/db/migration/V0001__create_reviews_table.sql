43c3d060ffd2dd8a50ab8cb6d798a8e14eac7438
axgiri <reiaogiri@gmail.com>
2025-08-30 16:36:18 +0200

    new collection, flyway migrations, hotfix

---
+++ src/main/resources/db/migration/V0001__create_reviews_table.sql
@@ -0,0 +1,25 @@
+-- Reviews table
+CREATE TABLE IF NOT EXISTS reviews (
+  id BIGSERIAL PRIMARY KEY,
+  version BIGINT,
+  author_id BIGINT NOT NULL,
+  rating BIGINT,
+  person_id BIGINT,
+  shop_id BIGINT,
+  comment TEXT
+);
+
+ALTER TABLE reviews
+  ADD CONSTRAINT fk_reviews_author
+  FOREIGN KEY (author_id) REFERENCES persons(id)
+  ON DELETE CASCADE;
+
+ALTER TABLE reviews
+  ADD CONSTRAINT fk_reviews_person
+  FOREIGN KEY (person_id) REFERENCES persons(id)
+  ON DELETE CASCADE;
+
+ALTER TABLE reviews
+  ADD CONSTRAINT fk_reviews_shop
+  FOREIGN KEY (shop_id) REFERENCES shops(id)
+  ON DELETE CASCADE;
