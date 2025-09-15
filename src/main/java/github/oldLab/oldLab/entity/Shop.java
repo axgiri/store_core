a2cb03eb7b549504ddcf6940c1329535290ee6e2
unknown <piligrimovivan2@gmail.com>
2025-09-16 00:06:20 +0500

    fix imports and postman collection

--- src/main/java/github/oldLab/oldLab/entity/Shop.java
+++ src/main/java/github/oldLab/oldLab/entity/Shop.java
@@ -4,7 +4,22 @@ import java.util.List;
 
 import github.oldLab.oldLab.Enum.CategoryEnum;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Version;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
